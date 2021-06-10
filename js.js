4e6bc6dc1ad4c41a8c0683707b18ac9af200d2d6
Jura <baykonuriy@gmail.com>
2021-06-10 07:20:22 +0300

    add less, add product page

--- js.js
+++ js.js
@@ -40,3 +40,19 @@ function removeClickClose(e) {
 }
 
 closeButton.addEventListener('click', openedMenu, removeClickClose, false)
+
+
+let listFilter = document.querySelectorAll('.filter__main__list')
+let selectItem = document.querySelectorAll('.select-item')
+let listWrapper = document.querySelector('.filter__main__list-wrapper');
+let filterHeadline = document.querySelectorAll('.filter__main__headline')
+
+
+function openFilter(e) {
+    e.currentTarget.parentElement.classList.toggle('opened-filter')
+    e.currentTarget.parentElement.classList.toggle('closed-filter')
+}
+
+for (i = 0; i <= selectItem.length; i++) {
+    selectItem[i].addEventListener('click', openFilter, false)
+}
